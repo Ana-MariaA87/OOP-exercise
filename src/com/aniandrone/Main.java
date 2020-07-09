d0942e9165276c0e6b87a749880cefb5c9849357
Ana-MariaA87 <66731161+Ana-MariaA87@users.noreply.github.com>
2020-07-09 17:49:35 +0200

    Update Main.java

--- src/com/aniandrone/Main.java
+++ src/com/aniandrone/Main.java
@@ -3,8 +3,7 @@ package com.aniandrone;
 public class Main {
 
     public static void main(String[] args) {
-        var employee = new Employee(
-                50_000, 20);
+        var employee = new Employee(50_000, 20);          
         int wage = employee.calculateWage(0);
         System.out.println(wage);
 
